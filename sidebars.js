adc85cde594f5c3053e98c7d3ff4ac4dc190cdb9
Marko Saric <34340819+metmarkosaric@users.noreply.github.com>
2020-06-09 12:17:29 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Account: ['introduction', 'get-started', 'website-settings', 'excluding'],
+    Account: ['introduction', 'get-started', 'website-settings', 'custom-domain', 'excluding'],
     Dashboard: ['guided-tour', 'top-referrers', 'top-pages', 'countries', 'devices'],
     Conversions: ['goal-conversions'],
     Referrers: ['search-console', 'twitter'],
