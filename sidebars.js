2024eefeac7c45b6bc5efd015e14d7142e236dc6
Marko Saric <34340819+metmarkosaric@users.noreply.github.com>
2020-06-10 16:31:25 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -3,9 +3,14 @@ module.exports = {
     "Welcome": ['introduction'],
     "Get Started": ['register-account', 'add-website', 'plausible-script', 'verify-integration'],
     "Website Settings": ['general', 'visibility', 'shared-links', 'goal-conversions', 'search-console', 'email-reports', 'custom-domain', 'delete-site-data', 'excluding'],
-    "Dashboard": ['guided-tour', 'top-referrers', 'top-pages', 'countries', 'devices'],
+    "Dashboard": ['guided-tour', 'top-referrers', 'top-pages', 'countries', 'devices',
+    {
+        type: 'category',
+        label: 'Referrers',
+        items: ['manual-link-tagging', 'twitter'],
+      },
+                  ],
     "Goal Conversions": ['pageview-goals', 'custom-event-goals'],
-    "Referrers": ['manual-link-tagging', 'twitter'],
     "Contribute": ['contribute'],
   },
 };
