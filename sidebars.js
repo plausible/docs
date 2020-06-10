8b42c57f997110ee9452f2bca8e3ba145341d903
Marko Saric <34340819+metmarkosaric@users.noreply.github.com>
2020-06-10 08:25:54 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    "Get Started": ['introduction', 'get-started', 'add-website', 'plausible-script', 'verify-integration'],
+    "Get Started": ['introduction', 'register-account', 'add-website', 'plausible-script', 'verify-integration'],
     "Settings": ['website-settings', 'custom-domain', 'excluding'],
     "Dashboard": ['guided-tour', 'top-referrers', 'top-pages', 'countries', 'devices'],
     "Conversions": ['goal-conversions', 'pageview-goals', 'custom-event-goals'],
