21048ec629d160c7fdd7d5d7364b19cbd8214635
Marko Saric <34340819+metmarkosaric@users.noreply.github.com>
2020-06-29 09:27:18 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     "Welcome": ['introduction'],
     "Get Started": ['register-account', 'add-website', 'plausible-script',  'spa-support', 'verify-integration'],
-    "Website Settings": ['website-settings', 'general', 'visibility', 'shared-links', 'goal-conversions', 'google-search-console-integration', 'email-reports', 'custom-domain', 'delete-site-data', 'excluding'],
+    "Website Settings": ['website-settings', 'general', 'visibility', 'shared-links', 'goal-conversions', 'google-search-console-integration', 'email-reports', 'custom-domain', 'export-stats', 'delete-site-data', 'excluding'],
     "Dashboard": ['guided-tour',
     {
         type: 'category',
