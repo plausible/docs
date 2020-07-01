00116323f382c76b7f65e58b52f6ce938507def6
Marko Saric <34340819+metmarkosaric@users.noreply.github.com>
2020-07-01 10:22:58 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,13 @@
 module.exports = {
   someSidebar: {
     "Welcome": ['introduction'],
-    "Get Started": ['register-account', 'add-website', 'plausible-script', 'wordpress-integration', 'spa-support', 'verify-integration'],
+    "Get Started": ['register-account', 'add-website', 
+                    {
+        type: 'category',
+        label: 'How to add the script to your website',
+        items: ['plausible-script', 'javascript-snippet', 'wordpress-integration'],
+      },
+                   'spa-support', 'verify-integration'],
     "Website Settings": ['website-settings', 'general', 'visibility', 'shared-links', 'goal-conversions', 'google-search-console-integration', 'email-reports', 'custom-domain', 'export-stats', 'delete-site-data', 'excluding'],
     "Dashboard": ['guided-tour',
     {
