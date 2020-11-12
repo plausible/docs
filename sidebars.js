46966e29e2b3ffec8e7b3238704535da4e8789e0
Marko Saric <34340819+metmarkosaric@users.noreply.github.com>
2020-11-12 11:57:14 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -5,7 +5,7 @@ module.exports = {
                     {
         type: 'category',
         label: 'How to add the script to your website',
-        items: ['plausible-script', 'javascript-snippet', 'wordpress-integration', 'webflow-integration', 'squarespace-integration', 'wix-integration', 'docusaurus-integration', 'spa-support', 'hash-based-routing'],
+        items: ['plausible-script', 'javascript-snippet', 'community-integrations', 'wordpress-integration', 'webflow-integration', 'squarespace-integration', 'wix-integration', 'docusaurus-integration', 'spa-support', 'hash-based-routing'],
       },
                    'verify-integration'],
     "Website Settings": ['website-settings', 'general', 'visibility', 'shared-links', 'goal-conversions', 'google-search-console-integration', 'email-reports', 'custom-domain', 'export-stats', 'reset-site-data', 'delete-site-data', 'excluding'],
@@ -19,6 +19,6 @@ module.exports = {
     "Goal Conversions": ['pageview-goals', 'custom-event-goals', 'outbound-link-click-tracking'],
     "Account Settings": ['trial-to-paid', 'change-email', 'reset-password', 'change-plan', 'cancel-subscription', 'delete-account'],
     "Self-Hosting": ['self-hosting', 'self-hosting-configuration'],
-    "Contribute": ['contribute', 'community-integrations', 'plausible-analytics-reviews', 'authors'],
+    "Contribute": ['contribute', 'plausible-analytics-reviews', 'authors'],
   },
 };
