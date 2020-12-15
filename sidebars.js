cf38fcb3dc675e6c19d6a4f6733efd6ce26fb631
Marko Saric <34340819+metmarkosaric@users.noreply.github.com>
2020-12-15 11:23:32 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,11 +1,11 @@
 module.exports = {
   someSidebar: {
     "Welcome": ['introduction'],
-    "Get Started": ['register-account', 'add-website',
+    "Get Started": ['register-account', 'add-website', 'plausible-script',
                     {
         type: 'category',
-        label: 'How to add the script to your website',
-        items: ['plausible-script', 'wordpress-integration', 'ghost-integration', 'webflow-integration', 'squarespace-integration', 'wix-integration', 'discourse-integration', 'docusaurus-integration', 'carrd-integration', 'spa-support', 'hash-based-routing', 'community-integrations'],
+        label: 'Integration guides',
+        items: ['wordpress-integration', 'ghost-integration', 'webflow-integration', 'squarespace-integration', 'wix-integration', 'discourse-integration', 'docusaurus-integration', 'carrd-integration', 'spa-support', 'hash-based-routing', 'community-integrations'],
       },
                 ],
     "Website Settings": ['website-settings', 'general', 'visibility', 'shared-links', 'goal-conversions', 'google-search-console-integration', 'email-reports', 'traffic-spikes', 'custom-domain', 'export-stats', 'reset-site-data', 'delete-site-data', 'excluding'],
