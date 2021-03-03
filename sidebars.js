2a2bd24aa24c9fad38be6375433d95de266f702c
Marko Saric <34340819+metmarkosaric@users.noreply.github.com>
2021-03-03 11:41:23 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     "Welcome": ['introduction'],
     "Get Started": ['register-account', 'add-website', 'plausible-script', 'integration-guides', 'your-plausible-experience'],
-    "Website Settings": ['website-settings', 'general', 'visibility', 'shared-links', 'goal-conversions', 'google-search-console-integration', 'email-reports', 'traffic-spikes', 'slack-reports', 'custom-domain', 'excluding-pages', 'export-stats', 'reset-site-data', 'delete-site-data', 'excluding'],
+    "Website Settings": ['website-settings', 'general', 'visibility', 'shared-links', 'google-search-console-integration', 'email-reports', 'traffic-spikes', 'slack-reports', 'custom-domain', 'excluding-pages', 'export-stats', 'reset-site-data', 'delete-site-data', 'excluding'],
     "Dashboard": ['guided-tour',
     {
         type: 'category',
