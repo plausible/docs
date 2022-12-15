30333807ee3247896a60f9b3ededead0809a7616
Marko Saric <34340819+metmarkosaric@users.noreply.github.com>
2022-12-15 14:57:55 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -3,7 +3,7 @@ module.exports = {
     "Get Started": ['introduction', 'register-account', 'add-website', 'plausible-script', 'script-extensions', 'integration-guides', 'troubleshoot-integration', 'your-plausible-experience'],
     "Website Settings": ['landing-page', 'website-settings', 'general', 'users-roles', 'visibility', 'shared-links', 'embed-dashboard', 'email-reports', 'traffic-spikes', 'slack-reports', 'excluding-pages', 'transfer-ownership', 'reset-site-data', 'delete-site-data', 'change-domain-name', 'excluding'],
     "Dashboard": ['guided-tour', 'realtime-dashboard', 'top-referrers', 'manual-link-tagging', 'google-search-console-integration', 'top-pages', 'countries', 'devices', 'filters-segments', 'google-analytics-import', 'export-stats', 'metrics-definitions', 'keyboard-shortcuts', 'dashboard-faq'],
-    "Goal Conversions": ['goal-conversions', 'outbound-link-click-tracking', 'file-downloads-tracking', 'error-pages-tracking-404', 'pageview-goals', 'custom-event-goals',
+    "Goals and Custom Events": ['goal-conversions', 'outbound-link-click-tracking', 'file-downloads-tracking', 'error-pages-tracking-404', 'pageview-goals', 'custom-event-goals',
       {
         type: 'category',
         label: 'Custom events CMS guides',
