c093e23c85a486ef889fbc79f5c824db7437f18a
Marko Saric <34340819+metmarkosaric@users.noreply.github.com>
2022-12-15 15:07:58 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     "Get Started": ['introduction', 'register-account', 'add-website', 'plausible-script', 'script-extensions', 'integration-guides', 'troubleshoot-integration', 'your-plausible-experience'],
     "Website Settings": ['landing-page', 'website-settings', 'general', 'users-roles', 'visibility', 'shared-links', 'embed-dashboard', 'email-reports', 'traffic-spikes', 'slack-reports', 'excluding-pages', 'transfer-ownership', 'reset-site-data', 'delete-site-data', 'change-domain-name', 'excluding'],
-    "Dashboard": ['guided-tour', 'realtime-dashboard', 'top-referrers', 'manual-link-tagging', 'google-search-console-integration', 'top-pages', 'countries', 'devices', 'filters-segments', 'google-analytics-import', 'export-stats', 'metrics-definitions', 'keyboard-shortcuts', 'dashboard-faq'],
+    "Stats Dashboard": ['guided-tour', 'realtime-dashboard', 'top-referrers', 'manual-link-tagging', 'google-search-console-integration', 'top-pages', 'countries', 'devices', 'filters-segments', 'google-analytics-import', 'export-stats', 'metrics-definitions', 'keyboard-shortcuts', 'dashboard-faq'],
     "Goals and Custom Events": ['goal-conversions', 'outbound-link-click-tracking', 'file-downloads-tracking', 'error-pages-tracking-404', 'pageview-goals', 'custom-event-goals',
       {
         type: 'category',
@@ -10,6 +10,7 @@ module.exports = {
         items: ['webflow-integration'],
       }
     ],
+    "API": ['stats-api',  'events-api', 'sites-api'],
     "Adblockers": ['proxy/introduction',
       {
         type: 'category',
@@ -17,9 +18,8 @@ module.exports = {
         items: ['proxy/guides/cloudflare', 'proxy/guides/akamai', 'proxy/guides/netlify', 'proxy/guides/vercel', 'proxy/guides/nextjs', 'proxy/guides/cloudfront', 'proxy/guides/nginx', 'proxy/guides/caddy'],
       }
     ],
-    "API": ['stats-api',  'events-api', 'sites-api'],
     "Account Settings": ['change-email', 'reset-password', 'dashboard-appearance', 'delete-account'],
-    "Manage Subscription": ['trial-to-paid', 'subscription-plans', 'change-plan', 'cancel-subscription', 'download-invoices', 'billing'],
+    "Billing and Subscription": ['trial-to-paid', 'subscription-plans', 'change-plan', 'cancel-subscription', 'download-invoices', 'billing'],
     "Self-Hosting": ['self-hosting', 'self-hosting-configuration'],
     "Contribute": ['contribute', 'plausible-analytics-reviews', 'authors'],
   },
