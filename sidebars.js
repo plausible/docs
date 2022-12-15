93e5a89d7e3cca41e9a7016252c600cd86f6d7b4
Marko Saric <34340819+metmarkosaric@users.noreply.github.com>
2022-12-15 12:22:26 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -3,7 +3,13 @@ module.exports = {
     "Get Started": ['introduction', 'register-account', 'add-website', 'plausible-script', 'script-extensions', 'integration-guides', 'troubleshoot-integration', 'your-plausible-experience'],
     "Website Settings": ['landing-page', 'website-settings', 'general', 'users-roles', 'visibility', 'shared-links', 'embed-dashboard', 'email-reports', 'traffic-spikes', 'slack-reports', 'excluding-pages', 'transfer-ownership', 'reset-site-data', 'delete-site-data', 'change-domain-name', 'excluding'],
     "Dashboard": ['guided-tour', 'realtime-dashboard', 'top-referrers', 'manual-link-tagging', 'google-search-console-integration', 'top-pages', 'countries', 'devices', 'filters-segments', 'google-analytics-import', 'export-stats', 'metrics-definitions', 'keyboard-shortcuts', 'dashboard-faq'],
-    "Goal Conversions": ['goal-conversions', 'pageview-goals', 'custom-event-goals', 'outbound-link-click-tracking', 'file-downloads-tracking', 'error-pages-tracking-404'],
+    "Goal Conversions": ['goal-conversions', 'pageview-goals', 'custom-event-goals', 'outbound-link-click-tracking', 'file-downloads-tracking', 'error-pages-tracking-404',
+                         {
+        type: 'category',
+        label: 'CMS Guides',
+        items: ['webflow-integration'],
+      }
+    ],
     "Adblockers": ['proxy/introduction',
       {
         type: 'category',
