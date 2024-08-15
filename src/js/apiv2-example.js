342fc1a939127d05bb115a876bfdbe397582d140
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2024-08-15 12:42:17 +0300

    Tabs

--- src/js/apiv2-example.js
+++ src/js/apiv2-example.js
@@ -1,5 +1,7 @@
-import React, {useState, useCallback, useRef, useMemo, useEffect} from 'react'
+import React, {useState, useCallback, useRef} from 'react'
 import { Editor } from '@monaco-editor/react'
+import Tabs from '@theme/Tabs'
+import TabItem from '@theme/TabItem'
 
 import Examples from './examples'
 import SCHEMA from './apiv2-json-schema.json'
@@ -8,13 +10,36 @@ const MIN_HEIGHT = 170
 const MAX_HEIGHT = 500
 
 export default function ApiV2Example({ request }) {
+  const [value, setValue] = useState(JSON.stringify(Examples[request], null, 2))
+
+  const onCodeChange = useCallback((value) => {
+    setValue(value)
+  })
+
+  return (
+    <Tabs>
+      <TabItem label="Request" value="request">
+        <JsonSchemaEditor
+          schema={SCHEMA}
+          defaultValue={value}
+          onChange={onCodeChange}
+        />
+      </TabItem>
+      <TabItem label="Response" value="response">
+        <pre>{value}</pre>
+      </TabItem>
+    </Tabs>
+  )
+}
+
+function JsonSchemaEditor({ defaultValue, schema, onChange }) {
   const [height, setHeight] = useState(MIN_HEIGHT)
   const editorRef = useRef()
 
   const onMount = useCallback((editor, monaco) => {
     editorRef.current = editor
 
-    monaco.languages.json.jsonDefaults.setDiagnosticsOptions(SCHEMA)
+    monaco.languages.json.jsonDefaults.setDiagnosticsOptions(schema)
     editor.onDidChangeModelContent(handleEditorChange)
     handleEditorChange()
   })
@@ -30,7 +55,7 @@ export default function ApiV2Example({ request }) {
     <Editor
       theme="vs-dark"
       language="json"
-      defaultValue={JSON.stringify(Examples[request], null, 2)}
+      defaultValue={defaultValue}
       height={height}
       options={{
         minimap: {
@@ -47,6 +72,7 @@ export default function ApiV2Example({ request }) {
         scrollBeyondLastLine: false,
       }}
       onMount={onMount}
+      onChange={onChange}
     />
   )
 }
