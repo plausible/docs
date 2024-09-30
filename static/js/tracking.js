5f4f59bcd20d4746d88cc4ec3b676b8b5d7398ea
Robert Joonas <robertjoonas16@gmail.com>
2024-09-30 15:01:05 +0200

    update script src

--- static/js/tracking.js
+++ static/js/tracking.js
@@ -2,7 +2,7 @@ const script = document.createElement('script')
 
 script.defer = true
 script.dataset.domain = 'plausible.io'
-script.src = 'https://plausible.io/js/script.pageview-props.js'
+script.src = 'https://plausible.io/js/script.pageview-props.pageleave.js'
 script.setAttribute('event-browser_language', navigator.language || navigator.userLanguage)
 
 document.getElementsByTagName('head')[0].appendChild(script)
