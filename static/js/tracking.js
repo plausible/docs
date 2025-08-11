a8a791b3f54295d770f0d47362b26ccb5efdbdad
Artur Pata <artur.pata@gmail.com>
2025-08-11 14:17:20 +0300

    Update dogfooding tracker URL to new format

--- static/js/tracking.js
+++ static/js/tracking.js
@@ -1,4 +1,4 @@
-var scriptSrc = "https://plausible.io/js/s-6_srOGVV9SLMWJ1ZpUAbG.js"
+var scriptSrc = "https://plausible.io/js/pa-6_srOGVV9SLMWJ1ZpUAbG.js"
 
 window.plausible=window.plausible||function(){(window.plausible.q=window.plausible.q||[]).push(arguments)},window.plausible.init=function(i){window.plausible.o=i||{}};var script=document.createElement("script");script.type="text/javascript",script.defer=!0,script.src=scriptSrc;var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(script,r);
 
